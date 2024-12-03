6cb5e7d5ac7bb08f755abfb70b05c5a7191fd44c
Max Flatline <testingon2022@gmail.com>
2024-12-03 22:36:19 +0300

    WIP Signed-off-by: Max Flatline <testingon2022@gmail.com>

---
+++ Drivers/FUTABA_8_MD_06INKM_Driver/inc/8_MD_06INKM.h
@@ -0,0 +1,35 @@
+// Header: Display module driver macros and functions
+// File Name: 8_MD_06INKM.h
+// Author: Hodnenko M.I.
+// Date: 03.12.2024
+
+#ifndef __8_MD_06INKM_H
+#define __8_MD_06INKM_H
+//====================================================================================================================//
+//====================================COMMAND SEQUENCES OF 8-MD-06INKM================================================//
+//====================================================================================================================//
+#define FUTABA_CMD_DCRAM_A_DATA_WRITE         (0x01U << 0x05U)
+
+#define FUTABA_CMD_CGRAM_DATA_WRITE           (0x01U << 0x06U)
+
+#define FUTABA_CMD_ADRAM_DATA_WRITE           (0x03U << 0x05U)
+
+#define FUTABA_CMD_URAM_DATA_WRITE            (0x01U << 0x07U)
+
+#define FUTABA_CMD_DISP_TIMING_DIGIT_SET      (0x07U << 0x05U)
+
+#define FUTABA_CMD_DIMMING_SET                (0x39U << 0x02U)
+
+#define FUTABA_CMD_DISPLAY_LIGHT_ON_OFF       (0x3AU << 0x02U)
+
+#define FUTABA_CMD_STANDBY_MODE_SET           (0x76U << 0x01U)
+
+//====================================================================================================================//
+//================================RECOMMENDED DISPLAY TIMING OF 8-MD-06INKM===========================================//
+//====================================================================================================================//
+
+#define FUTABA_CMD_RECOMMENDED_DISPLAY_TIMING (0x07U << 0x00U)
+
+
+#endif /* __8_MD_06INKM_H */
+
---
+++ Drivers/FUTABA_8_MD_06INKM_Driver/src/8_MD_06INKM.c
@@ -0,0 +1,5 @@
+// Header: Display driver function definitions
+// File Name: 8_MD_06INKM.c
+// Author: Hodnenko M.I.
+// Date: 03.12.2024
+
